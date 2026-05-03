e3431ee62f2bcc2f4ed7070e334dc41c2af15b3c
Aleksei Elivanov <supersilver3310@gmail.com>
2026-05-03 13:42:03 -0600

    Adjust the about menu

--- src/ui/Menu.js
+++ src/ui/Menu.js
@@ -393,7 +393,7 @@ export class Menu {
             'PushThemOut 2 — 2026',
             '',
             'Programmer:        silver3310',
-            'Graphic designer: elivlikswer',
+            'Graphic designer:  elivlikswer',
             'Sound designer:    wsocha',
         ];
         let y = 230;
